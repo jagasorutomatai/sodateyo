b322ed7c6ae8b084927c6ef2a679876e415335d1
jagasorutomatai <jagasorutomatai@gmail.com>
2020-06-02 21:58:17 +0900

    👍  gitignore反映 issue #2 gitignore反映

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  root 'static_pages#home'
-  get  '/help', to: 'static_pages#help'
-  get  '/about', to: 'static_pages#about'
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
-  resources :users
-end
--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    name { 'testuser' }
-    email { 'testuser@sample.co.jp' }
-    password { 'password' }
-    password_confirmation { 'password' }
-  end
-end
