d0c7a0c3ba821a213a677263493cf814f44aea9d
jagasorutomatai <jagasorutomatai@gmail.com>
2020-06-28 14:01:27 +0900

    ✅ ファロー関連のテストを追加 issue #37 ・controllerのテストを追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,4 +1,6 @@
 class RelationshipsController < ApplicationController
+    before_action :logged_in_user
+
     def create
         @user = User.find(params[:followed_id])
         current_user.follow(@user)
@@ -17,3 +19,4 @@ class RelationshipsController < ApplicationController
         end
     end
 end
+
