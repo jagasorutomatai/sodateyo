3d83d6961f6dd57bd30f6312d9fef50286d386ca
jagasorutomatai <jagasorutomatai@gmail.com>
2020-07-19 15:58:49 +0900

    🎉 管理者機能を追加

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,65 @@
+module Admin
+  class UsersController < Admin::ApplicationController
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:success] = 'アカウントの登録が完了しました'
+        redirect_to admin_users_url
+      else
+        flash[:alert] = '不正な登録内容です'
+        redirect_to new_admin_user_url
+      end
+    end
+    # Overwrite any of the RESTful controller actions to implement custom behavior
+    # For example, you may want to send an email after a foo is updated.
+    #
+    # def update
+    #   super
+    #   send_foo_updated_email(requested_resource)
+    # end
+
+    # Override this method to specify custom lookup behavior.
+    # This will be used to set the resource for the `show`, `edit`, and `update`
+    # actions.
+    #
+    # def find_resource(param)
+    #   Foo.find_by!(slug: param)
+    # end
+
+    # The result of this lookup will be available as `requested_resource`
+
+    # Override this if you have certain roles that require a subset
+    # this will be used to set the records shown on the `index` action.
+    #
+    # def scoped_resource
+    #   if current_user.super_admin?
+    #     resource_class
+    #   else
+    #     resource_class.with_less_stuff
+    #   end
+    # end
+
+    # Override `resource_params` if you want to transform the submitted
+    # data before it's persisted. For example, the following would turn all
+    # empty values into nil values. It uses other APIs such as `resource_class`
+    # and `dashboard`:
+    #
+    # def resource_params
+    #   params.require(resource_class.model_name.param_key).
+    #     permit(dashboard.permitted_attributes).
+    #     transform_values { |value| value == "" ? nil : value }
+    # end
+
+    # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
+    # for more information
+    def permitted_attributes
+      dashboard.permitted_attributes << %w(password password_confirmation)
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  end
+end
